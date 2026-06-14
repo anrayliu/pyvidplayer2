0b6a13beb4a831649c528b473689b9e70981d69b
ree1261 <anrayliu@gmail.com>
2026-06-14 16:38:54 -0400

    chore: update audio tracks example

--- examples/audio_devices_demo.py
+++ examples/audio_devices_demo.py
@@ -5,6 +5,7 @@ This demo shows how the audio output device can be specified for each video
 # Sample videos can be found here:
 # https://github.com/anrayliu/pyvidplayer2-test-resources/tree/main/resources
 
+from pprint import pprint
 import sounddevice
 from pyvidplayer2 import Video
 
@@ -14,16 +15,19 @@ from pyvidplayer2 import Video
 # to see how the output device can be specified during mixer initialization
 
 # FOR SOUNDDEVICE
-# see instructions below
 
 # find the index of the device you want
-# not all hostapis work, but MME for Windows should be fine
+# not all hostapis work well. I've found success with
+# MME and WASAPI (v0.9.34+), but your experience may vary
 
 # e.g
 # 0 Microsoft Sound Mapper - Input, MME (2 in, 0 out)
 # 1 Microsoft Sound Mapper - Output, MME (0 in, 2 out)
 
+print("Host APIs:\n")
+pprint(sounddevice.query_hostapis())
 
+print("\nDevices:\n")
 print(sounddevice.query_devices())
 
 # replace None with the index of the chosen device (first number listed by sd)
